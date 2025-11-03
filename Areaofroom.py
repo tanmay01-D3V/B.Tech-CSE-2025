40859c57c69de1cf0b5a2f572d68f8c733970fc9
Tanmay Vijay Sherkar <tanmayvijaysherkar@Tanmays-MacBook-Air.local>
2025-11-03 13:59:27 +0530

    first commit

---
+++ Areaofroom.py
@@ -0,0 +1,4 @@
+Length=(float(input("Enter the length of the room in feet: ")))
+Width=(float(input("Enter the width of the room in feet: ")))
+Area=Length*Width
+print("The area of the room is", Area, "square feet.")
---
+++ variable.py
@@ -0,0 +1,13 @@
+price = 100
+tax = 10
+
+x = 10
+y = "greeks"
+z = 20
+price = 100
+tax = 10
+
+x = 10
+y = "greeks"
+z = 20
+w = z
